aae7f5eab032ad7fcdf46d44590315695eaed4fe
Stewart Young <stewartyoung97@gmail.com>
2020-05-31 09:48:56 +0100

    add gale shapley deffered acceptance algorithm

--- replicatorDynamics.py
+++ replicatorDynamics.py
@@ -1,10 +1,12 @@
 import numpy as np
 
+# two rows of A
 payoffMatrix1 = np.array([[0.3, 0],[0.5, 0.1]])
 A = payoffMatrix1
 strategyProfile1 = np.array([0.4, 0.6])
 x = strategyProfile1
 
+# two columns of B
 payoffMatrix2 = np.array([[0.3, 0],[0.5, 0.1]])
 B = payoffMatrix2
 strategyProfile2 = np.array([0.5, 0.5])
@@ -16,7 +18,6 @@ for step in range(EVOLUTION_STEPS):
     print("********* Step {} *********".format(step+1))
     print("x at step {}: {}".format(step+1, x))
     print("y at step {}: {}".format(step+1, y))
-    print(y[:, np.newaxis])
 
     Ay = np.sum(A*y[:, np.newaxis], axis=1)
     xTAy = np.transpose(x).dot(Ay)
@@ -24,14 +25,10 @@ for step in range(EVOLUTION_STEPS):
     print("xTAy at step {}: {}".format(step+1, xTAy))
 
     xDot = np.zeros(A.shape[0])
-    print
     for i in range(A.shape[0]):
         xDot[i] = round(x[i] * (Ay[i]-xTAy),6)
         print("Population change for strategy {} of player {}: {}".format(i+1, 1, xDot[i]))
     
-    
-    print(x)
-    print(B)
     xTB = np.sum(x*B, axis=1)
     xTBy = round(xTB.dot(y),6)
     print("xTB at step {}: {}".format(step+1, xTB))
